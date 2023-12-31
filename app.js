0c25854010900ee5b7f37c4c96710368029a8d8a
LevelLawrence-FS <lhlawrence@student.fullsail.edu>
2023-12-30 20:59:46 -0500

    play round working

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const gameChoices = ["rock", "paper", "scissors"];
+
 // computer choices
 const getComputerChoice = () => {
-  const choices = Math.floor(Math.random() * (3 - 0) + 0);
+  const choices = Math.floor(Math.random() * 3);
   return gameChoices[choices];
 };
 
-console.log(getComputerChoice());
+const computerChoice = getComputerChoice();
+
+const playRound = (playerSelection, computerChoice) => {
+  if (playerSelection === computerChoice) {
+    return `It's a tie! playerChoice: ${playerSelection}, cpuChoice: ${computerChoice}!`;
+  }
+
+  if (
+    (playerSelection === "rock" && computerChoice === "scissors") ||
+    (playerSelection === "paper" && computerChoice === "rock") ||
+    (playerSelection === "scissors" && computerChoice === "paper")
+  ) {
+    return `player wins!: player - ${playerSelection} beats cpu - ${computerChoice}`;
+  } else {
+    return `you lose!: cpu - ${computerChoice} beats player - ${playerSelection}`;
+  }
+};
+
+console.log(playRound("paper", computerChoice));
