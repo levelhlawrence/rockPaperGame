62325d9d7109edf6d22738c193b7e183fa888365
LevelLawrence-FS <lhlawrence@student.fullsail.edu>
2024-01-02 21:52:39 -0500

    changed to a class based system

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
-const gameChoices = ["rock", "paper", "scissors"];
+class rockPaperScissorsGame {
+  constructor() {
+    this.score = {
+      player: 0,
+      cpu: 0,
+      tie: 0,
+    };
+    this.choices = ["rock", "paper", "scissors"];
+    this.gameSection = document.querySelector("#start-game-section");
+    this.startGame = document.querySelector("#start-game");
+    this.bgAudio = document.querySelector("#background-audio");
+    this.bgAudio.volume = "0.01";
 
-// computer choices
-const getComputerChoice = () => {
-  const choices = Math.floor(Math.random() * 3);
-  return gameChoices[choices];
-};
-
-const computerChoice = getComputerChoice();
-
-const playRound = (playerSelection, computerChoice) => {
-  if (playerSelection === computerChoice) {
-    return `It's a tie! playerChoice: ${playerSelection}, cpuChoice: ${computerChoice}!`;
+    // querying player buttons
+    this.playerBtns = document.querySelectorAll(".player-input-btns");
   }
 
-  if (
-    (playerSelection === "rock" && computerChoice === "scissors") ||
-    (playerSelection === "paper" && computerChoice === "rock") ||
-    (playerSelection === "scissors" && computerChoice === "paper")
-  ) {
-    return `player wins!: player - ${playerSelection} beats cpu - ${computerChoice}`;
-  } else {
-    return `you lose!: cpu - ${computerChoice} beats player - ${playerSelection}`;
+  randomCpu() {
+    return this.choices[Math.floor(Math.random() * 3)];
   }
-};
+  playerChoices() {
+    this.playerBtns.forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        let playerInput = e.target.id;
+        this.playRound(this.randomCpu(), playerInput);
+        console.log(this.score);
+      });
+    });
+  }
 
-console.log(playRound("paper", computerChoice));
+  playRound(cpuChoice, playerPicks) {
+    if (cpuChoice === playerPicks) {
+      this.score["tie"] += 1;
+      console.log(`${cpuChoice}, ${playerPicks}`);
+      console.log(this.score);
+    }
+  }
+
+  startTheGame() {
+    let isStarted = false;
+    this.startGame.addEventListener("click", () => {
+      this.gameSection.style.display = "none";
+      isStarted = !isStarted;
+      // console.log(this.score);
+    });
+  }
+}
+
+const game = new rockPaperScissorsGame();
+game.startTheGame();
+game.playerChoices();
